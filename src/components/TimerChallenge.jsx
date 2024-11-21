d71b2976d1da6f2a49c7f91442882065d0ebe0e6
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-21 17:17:30 +0000

    Add stop timer functionality

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     setTimerStarted(true);
-    setTimeout(() => {
+
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    setTimerStarted(false);
+    clearTimeout(timer.current);
   }
 
   return (
@@ -18,7 +26,7 @@ export default function TimerChallenge({ title, targetTime }) {
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
-      <button onClick={handleStart}>
+      <button onClick={timerStarted ? handleStop : handleStart}>
         {timerStarted ? "Stop" : "Start"} Challenge
       </button>
       <p className={timerStarted ? "active" : undefined}>
